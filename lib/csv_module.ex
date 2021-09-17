d86cfcb2aa762033472ddc0616a7fd87a942265f
鈴木拓実 <suzukitakumi@suzukitakuminoMacBook-Pro.local>
2021-09-17 14:26:57 +0900

    issue-2

--- lib/csv_module.ex
+++ lib/csv_module.ex
@@ -1,4 +1,5 @@
 defmodule CsvModule do
+  alias CsvModule.Customeres
   def read_csv(csv_file) do
     File.read!(csv_file)
   end
@@ -6,13 +7,23 @@ defmodule CsvModule do
   def processing_csv(data) do
     [field_name | value] = String.split(data, "\r\n")
 
-    field = String.split(field_name, ",")
+    field =
+    String.split(field_name, ",")
+    |> Enum.map(fn name ->
+      case name do
+        "取引先番号" -> :customer_number
+        "取引先名称" -> :customer_name
+        "与信スコア" -> :credit_score
+        "都道府県" -> :prefectures
+      end
+    end)
 
     Enum.map(value, &String.split(&1, ","))
     |> Enum.map(&Enum.into(Enum.zip(field, &1), %{}))
+    |> Enum.map(&Map.update(&1, :credit_score, 0, fn number -> String.to_integer(number) end))
   end
 
   def main(csv_file) do
-    read_csv(csv_file) |> processing_csv()
+    read_csv(csv_file) |> processing_csv() |> Enum.each(&Customeres.insert(&1))
   end
 end
